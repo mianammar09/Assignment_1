923bd63f1746a4ec88547a8e48835603de97b111
Mian Ammar Wattoo <ammarajmal4097@gmail.com>
2023-09-21 17:03:50 +0500

    Final Version

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,15 +2,28 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.menu);
+        setContentView(R.layout.activity_main);
+        Button btn = findViewById(R.id.loginBtn);
+        btn.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view){
+                Intent intent =  new Intent(MainActivity.this,MainActivity2.class);
+                startActivity(intent);
+                
+            }
+        });
+
+
     }
 }
